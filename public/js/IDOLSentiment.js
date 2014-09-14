e0c8befe41fab620242fc9e8cb16e7a3f650ed81
Dan Treiman <dan.treiman@gmail.com>
2014-09-13 17:55:20 -0700

    Added IDOLSentiment.js and parse SDK.

---
+++ public/js/IDOLSentiment.js
@@ -0,0 +1,46 @@
+/**
+ * Created by dtreiman on 9/13/14.
+ */
+
+
+/**
+ * @param text The text to analyze.
+ * @param success
+ * @param error
+ */
+function getSentiment(text, callback, error) {
+
+
+};
+
+
+/**
+ {
+   "positive": [
+     {
+       "sentiment": "like",
+       "topic": "oranges",
+       "score": 0.7176687736973063,
+       "original_text": "I like oranges",
+       "original_length": 14,
+       "normalized_text": "I like oranges",
+       "normalized_length": 14
+     }
+   ],
+   "negative": [
+     {
+       "sentiment": "don't like",
+       "topic": "bread",
+       "score": -0.7786856817316798,
+       "original_text": "but I don't like bread",
+       "original_length": 22,
+       "normalized_text": "but I don't like bread",
+       "normalized_length": 22
+     }
+   ],
+   "aggregate": {
+     "sentiment": "neutral",
+     "score": -0.03050845401718677
+   }
+ }
+ */
